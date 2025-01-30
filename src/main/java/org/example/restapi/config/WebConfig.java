9667de13f695b48412266142f9b700432304a7b0
Zhan Li <lizhan_mit@outlook.com>
2025-01-30 17:55:30 +0800

    Add pageable handler to limit the page size.

---
+++ src/main/java/org/example/restapi/config/WebConfig.java
@@ -0,0 +1,23 @@
+package org.example.restapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${max.page.size}")
+    private int maxPageSize;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setMaxPageSize(maxPageSize);
+        resolvers.add(pageableResolver);
+    }
+}
